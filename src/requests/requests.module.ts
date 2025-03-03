00fa3876ea062a2d411f9015a3b322659d4b942b
walid lhaila <walidlhaila00@gmail.com>
2025-03-03 13:53:15 +0000

    Feat: CREATE NEW ENDPOINT ACCEPT REQUEST THEN CREATE NEW CONVERSATION

--- src/requests/requests.module.ts
+++ src/requests/requests.module.ts
@@ -5,11 +5,22 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from '../auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { Requests, RequestsSchema } from './entity/requests.entity';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Requests.name, schema: RequestsSchema },
+    ]),
+    ClientsModule.register([
+      {
+        name: 'Messages-Service',
+        transport: Transport.TCP,
+        options: {
+          host: '127.0.0.1',
+          port: 5000,
+        },
+      },
     ]),
     AuthModule,
     ConfigModule,
